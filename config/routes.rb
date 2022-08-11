772feef5a2aaa8bfa992513f3940f0ffcaae641d
EC2 Default User <ec2-user@ip-172-31-10-63.ec2.internal>
2022-08-11 14:48:35 +0000

    [20220811]

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
- }
- 
- devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
  }
  
  namespace :admin do
@@ -22,9 +12,17 @@ Rails.application.routes.draw do
   resources :orders
  end
  
+ devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+ }
+ 
  scope module: :public do
   root to: 'homes#top'
-  get 'homes/about'
+  get 'about', to: 'homes#about'
   resources :customers
+  get 'customers/unsubscribe', to: 'customers#unsubscribe'
+  resources :orders
+  get 'orders/completed', to: 'orders#completed'
  end
 end
