5e12184c4f4e368fdad64f1c0820c00b0a81a940
EC2 Default User <ec2-user@ip-172-31-10-63.ec2.internal>
2022-08-18 16:01:26 +0000

    [20220818]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
@@ -13,11 +14,16 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
   end
   
   private
   
   def orders_params
-    params.require(:order).permit(:customer_id,:shipping_postal_code,:shipping_address,:shipping_name,:postage,:billing_amount,:payment_method,:status)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
